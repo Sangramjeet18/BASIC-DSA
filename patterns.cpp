87ba8aea827f73a57990eefc42b556cc68bb30d1
Sangramjeet18 <sangramjeet18@gmail.com>
2026-08-07 13:52:03 +0530

    Initial commit

---
+++ patterns.cpp
@@ -0,0 +1,66 @@
+#include<iostream>
+using namespace std;
+//SQUARE
+/*int square(int n){
+    int num=1;
+    for(int i=0;i<n;i++){
+        for (int j=0;j<n;j++){
+            cout<<num<<"  ";
+            num++;
+        }
+        cout<<endl;
+    }
+    
+}
+
+//TRIANGLE    *
+              **
+              ***
+               
+int triangle1(int n){
+    for(int i=0;i<n;i++){
+        for(int j=0;j<i+1;j++){
+            cout<<"\t*";
+        }
+        cout<<endl;
+    }
+}
+ 
+//TRIANGLE INVERTED
+int triangle2(int n){
+    for(int i=0;i<n;i++){
+        for(int j=0;j<i+1;j++){
+            cout<<"  ";
+        }
+        for(int j=0;j<n-i;j++){
+            cout<<(i+1);
+        }
+        cout<<endl;
+    }
+}
+//PYRAMID
+void triangle3(int n)
+{
+    for(int i=0; i<n; i++)
+    {
+        for(int j=0; j<n-i-1; j++)
+            cout << " ";
+
+        for(int j=1; j<=i+1; j++)
+            cout << j;
+
+        for(int j=i; j>=1; j--)
+            cout << j;
+
+        cout << endl;
+    }
+} 
+
+int main(){
+    int x;
+    cout<<"ENTER:"<<endl;
+    cin>>x;
+    triangle3(x);
+    return 0 ;
+}
+*/
