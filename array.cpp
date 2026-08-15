69cf7b956fc5dccc962ba700f95a69ca3f79473b
Sangramjeet18 <sangramjeet18@gmail.com>
2026-08-15 20:40:51 +0530

    UPDATED

--- array.cpp
+++ array.cpp
@@ -1,6 +1,6 @@
 #include<iostream>
 using namespace std;
-
+/*
 int main(){
     int n;
     cout<<"ENTER NUMBER:"<<endl;
@@ -24,6 +24,33 @@ int main(){
     
    
 
+    return 0;
+
+}
+*/
+//LINEAR SEARCH
+int main(){
+    int n,x;
+    cout<<"ENTER:"<<endl;
+    cin>>n;
+    cout<<"GIVE THE NUMBER:"<<endl;
+    int arr[n];
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<endl;
+    }
+    cout<<"ENTER THE NUMBER WANT TO SEARCH:"<<endl;
+    cin>>x;
+    for(int i=0;i<n;i++){
+        if(arr[i]==x){
+            cout<<"FOUND"<<arr[i]<<endl;
+        }
+        else{
+            cout<<"NOT FOUND at"<<arr[i]<<endl;
+        }
+    }
     return 0;
 
 }
